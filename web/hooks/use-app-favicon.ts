1bb11bc168385e4794b5b1a7924ce9aa27568431
liujing <liuj@soyuan.com.cn>
2025-09-02 11:45:43 +0800

    1

--- web/hooks/use-app-favicon.ts
+++ web/hooks/use-app-favicon.ts
@@ -10,7 +10,7 @@ type UseAppFaviconOptions = {
   icon_background?: string | null
   icon_url?: string | null
 }
-
+ 
 export function useAppFavicon(options: UseAppFaviconOptions) {
   const {
     enable = true,
